95efded729d47b94822b807f0d74c5c0a45fb219
ananathmukesh <ananthmukesh625@gmail.com>
2024-02-02 17:08:38 +0530

    first commit

---
+++ knexfileLive.js
@@ -0,0 +1,50 @@
+// knexfile.js
+require('dotenv').config();
+
+module.exports = {
+  client: 'mssql',
+  connection: {
+    host: 'seconddb.cvqgq6ou6nh3.ap-south-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'leo45gkm',
+    database: 'test_project',
+    charset: 'utf8',
+    options: {
+      encrypt: true, // For Azure SQL Database
+      trustServerCertificate: true, // Added for SSL trust
+    },
+    
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: './migrations',
+  },
+  seeds: {
+    directory: './seeds',
+  },
+};
+
+
+
+// Locall
+// // knexfile.js
+// require('dotenv').config();
+
+// module.exports = {
+//   client: 'mysql',
+//   connection: {
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'kodukku_test_project',
+//     charset: 'utf8',
+//   },
+  
+//   migrations: {
+//     tableName: 'knex_migrations',
+//     directory: './migrations',
+//   },
+//   seeds: {
+//     directory: './seeds',
+//   },
+// };
---
+++ util/webSocket.js
@@ -0,0 +1,30 @@
+// socket.js
+
+const { Server } = require("socket.io");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+
+io.on('connection', (socket) => {
+  console.log(`User Connected ${socket.id}`);
+})
+
+
+
+server.listen(8002, () => {
+  console.log('Socket.IO server is running on port 8002');
+});
+
+module.exports = io;

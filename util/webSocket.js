5c22ca03ea521fa354db1c2f4bcc23a7a1833253
ananathmukesh <ananthmukesh625@gmail.com>
2024-02-02 17:55:36 +0530

    add frontend live url

--- util/webSocket.js
+++ util/webSocket.js
@@ -11,10 +11,18 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://c-chat-frontend-mu.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
+
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
 
 io.on('connection', (socket) => {

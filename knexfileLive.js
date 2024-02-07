c8d648e538d4c2e68a303feeb5dae55e34cd396c
ananathmukesh <154673797+ananathmukesh@users.noreply.github.com>
2024-02-07 15:53:26 +0530

    Update knexfileLive.js

--- knexfileLive.js
+++ knexfileLive.js
@@ -1,29 +1,29 @@
 // // knexfile.js
 // require('dotenv').config();
 
-module.exports = {
-  client: 'mssql',
-  connection: {
-    host: 'seconddb.cvqgq6ou6nh3.ap-south-1.rds.amazonaws.com',
-    user: 'admin',
-    password: 'leo45gkm',
-    database: 'test_project',
-    charset: 'utf8',
-    port:1433,
-    options: {
-      encrypt: true, // For Azure SQL Database
-      trustServerCertificate: true, // Added for SSL trust
-    },
+// module.exports = {
+//   client: 'mssql',
+//   connection: {
+//     host: 'seconddb.cvqgq6ou6nh3.ap-south-1.rds.amazonaws.com',
+//     user: 'admin',
+//     password: 'leo45gkm',
+//     database: 'test_project',
+//     charset: 'utf8',
+//     port:1433,
+//     options: {
+//       encrypt: true, // For Azure SQL Database
+//       trustServerCertificate: true, // Added for SSL trust
+//     },
     
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-    directory: './migrations',
-  },
-  seeds: {
-    directory: './seeds',
-  },
-};
+//   },
+//   migrations: {
+//     tableName: 'knex_migrations',
+//     directory: './migrations',
+//   },
+//   seeds: {
+//     directory: './seeds',
+//   },
+// };
 
 
 

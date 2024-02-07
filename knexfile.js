627e1515dd8e8ccf0252538bcf0632cad1422cbf
ananathmukesh <ananthmukesh625@gmail.com>
2024-02-07 15:51:39 +0530

    db config

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 module.exports = {
   client: 'mssql',
   connection: {
-    server: 'DESKTOP-SKTGI2Q',
-    user: 'mukeshkanna',
-    password: 'leo45gkm',
-    database: 'kodukku_test_project',
+    server: 'sql.freedb.tech',
+    user: 'freedb_liveroot',
+    password: 'dn%&s6fN8*V#K4m',
+    database: 'freedb_kodukku_test_project',
     charset: 'utf8',
    
     options: {
--- knexfileLive.js
+++ knexfileLive.js
@@ -4,17 +4,16 @@ require('dotenv').config();
 module.exports = {
   client: 'mssql',
   connection: {
-    host: 'seconddb.cvqgq6ou6nh3.ap-south-1.rds.amazonaws.com',
-    user: 'admin',
-    password: 'leo45gkm',
-    database: 'test_project',
+    server: 'sql.freedb.tech',
+    user: 'freedb_liveroot',
+    password: 'dn%&s6fN8*V#K4m',
+    database: 'freedb_kodukku_test_project',
     charset: 'utf8',
-    port:1433,
+   
     options: {
-      encrypt: true, // For Azure SQL Database
-      trustServerCertificate: true, // Added for SSL trust
+      encrypt: false, 
+      
     },
-    
   },
   migrations: {
     tableName: 'knex_migrations',

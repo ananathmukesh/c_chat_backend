405062b929321680d53865adc9d3f79ecfe7c91e
ananathmukesh <154673797+ananathmukesh@users.noreply.github.com>
2024-02-07 15:48:16 +0530

    commit the database

--- knexfileLive.js
+++ knexfileLive.js
@@ -1,29 +1,29 @@
-// knexfile.js
-require('dotenv').config();
+// // knexfile.js
+// require('dotenv').config();
 
-module.exports = {
-  client: 'mssql',
-  connection: {
-    host: 'seconddb.cvqgq6ou6nh3.ap-south-1.rds.amazonaws.com',
-    user: 'admin',
-    password: 'leo45gkm',
-    database: 'test_project',
-    charset: 'utf8',
-    port:1433,
-    options: {
-      encrypt: true, // For Azure SQL Database
-      trustServerCertificate: true, // Added for SSL trust
-    },
+// module.exports = {
+//   client: 'mssql',
+//   connection: {
+//     host: 'seconddb.cvqgq6ou6nh3.ap-south-1.rds.amazonaws.com',
+//     user: 'admin',
+//     password: 'leo45gkm',
+//     database: 'test_project',
+//     charset: 'utf8',
+//     port:1433,
+//     options: {
+//       encrypt: true, // For Azure SQL Database
+//       trustServerCertificate: true, // Added for SSL trust
+//     },
     
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-    directory: './migrations',
-  },
-  seeds: {
-    directory: './seeds',
-  },
-};
+//   },
+//   migrations: {
+//     tableName: 'knex_migrations',
+//     directory: './migrations',
+//   },
+//   seeds: {
+//     directory: './seeds',
+//   },
+// };
 
 
 
@@ -49,3 +49,31 @@ module.exports = {
 //     directory: './seeds',
 //   },
 // };
+
+
+// knexfile.js
+require('dotenv').config();
+
+module.exports = {
+  client: 'mssql',
+  connection: {
+    server: 'sql.freedb.tech',
+    user: 'freedb_liveroot',
+    password: 'dn%&s6fN8*V#K4m',
+    database: 'freedb_kodukku_test_project',
+    charset: 'utf8',
+   
+    options: {
+      encrypt: false, 
+      
+    },
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: './migrations',
+  },
+  seeds: {
+    directory: './seeds',
+  },
+};
+
